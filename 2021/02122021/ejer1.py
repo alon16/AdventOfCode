24b03fea87cad55704943fd3562c13ec686ecff5
Alondra <alondra.ruiz@hotaka.io>
2021-12-06 12:18:02 -0600

    ejer dia 3

--- 2021/02122021/ejer1.py
+++ 2021/02122021/ejer1.py
@@ -1,6 +1,6 @@
 depth=0
 forward=0
-with open("input.txt","r") as archivo:
+with open("02122021/input.txt","r") as archivo:
     for linea in archivo:
         x= linea.split(' ')
         if x[0] == "up":
---
+++ 2021/03122021/ejer1.py
@@ -0,0 +1,28 @@
+def main():
+    result,unos,ceros=[],0,0
+    with open("03122021/input.txt","r") as archivo:
+        for linea in archivo:
+            list=[]
+            list.extend(linea.strip())
+            result.append(list)
+        gamma=""
+        elipson=""
+    for x in range(12):
+        for i in range(len(result)):
+            if(int(result[i][x])== 1):
+                unos+=1
+            else: 
+                ceros+=1
+        if unos>ceros:
+            gamma= gamma+"1"
+        else:
+            gamma= gamma+"0"
+        unos=0
+        ceros=0
+    for item in gamma:
+        if item == "1":
+            elipson= elipson+"0"
+        else :
+            elipson= elipson+"1"
+    print(int(gamma, 2)*int(elipson,2))
+main()
